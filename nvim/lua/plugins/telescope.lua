a91c445872f7e92120c390f1d8b40335fefeed8f
mgc <chagel@gmail.com>
2025-03-29 16:08:04 -0700

    Feat: Add buffer deletion mappings to Telescope pickers

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,12 +2,25 @@ return {
   'nvim-telescope/telescope.nvim',
   dependencies = { 'nvim-lua/plenary.nvim' },
   opts = function()
+    local actions = require('telescope.actions')
     return {
       defaults = {
         prompt_position = 'top',
         layout_strategy = 'horizontal',
         layout_config = { height = 0.5 },
       },
+      pickers = {
+        buffers = {
+          mappings = {
+            i = {
+              ["<c-d>"] = actions.delete_buffer,
+            },
+            n = {
+              ["d"] = actions.delete_buffer,
+            },
+          },
+        },
+      },
     }
-	end	
+	end
 }
