31e3bc78358809a09dc757548bc97ded73028d67
mgc <chagel@gmail.com>
2025-06-11 15:03:32 -0700

    Fix: migrate avante providers
    Enhance the coding plugin by adding support for multiple providers, including `claude`, `openai`, and `ollama`. This update reorganizes the provider configuration structure, allowing for more detailed options like custom endpoints, model specifications, timeouts, and additional request parameters. These changes enable better customization and flexibility for different AI models and use cases.

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -87,12 +87,36 @@ return {
     event = "VeryLazy",
     lazy = false,
     opts = {
-      provider = "claude", -- "claude" or "openai" or "azure"
-      claude = {
-        endpoint = "https://api.anthropic.com",
-        model = "claude-3-7-sonnet-20250219",
-        temperature = 0,
-        max_tokens = 4096,
+      providers = {
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-3-7-sonnet-20250219",
+          extra_request_body = {
+            temperature = 0,
+            max_tokens = 4096,
+          }
+        },
+        openai = {
+          endpoint = "https://api.openai.com/v1",
+          model = "gpt-4o", -- your desired model (or use gpt-4o, etc.)
+          timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+          extra_request_body = {
+            temperature = 0,
+            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
+            reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+          },
+        },
+        ollama = {
+          endpoint = "http://127.0.0.1:11434",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            options = {
+              temperature = 0.75,
+              num_ctx = 20480,
+              keep_alive = "5m",
+            },
+          },
+        },
       },
       highlights = {
         diff = {
