8fdd6433a481d7de4a68f10e5bd7192159f9e2e0
mgc <chagel@gmail.com>
2025-11-11 14:11:26 -0800

    Refactor: Enhance makefile to allow multiple independent
    Updated makefile targets to use double colons for better specificity and style consistency. Removed trailing spaces in comments for a cleaner look.

--- base.mk
+++ base.mk
@@ -2,34 +2,34 @@ DOTS := $(shell ls dots)
 CONFIGS := $(shell ls configs)
 BASE := $(shell pwd)
 
-all: init setup 
+all: init setup
 
-init: 
+init::
 	## make .config folder
 	@mkdir -pv ${HOME}/.config
 
-setup:
+setup::
 	## link dotfile under ${HOME} and ${XDG_CONFIG_HOME}
 	@for item in $(DOTS); do ln -vsfn ${BASE}/dots/$$item ${HOME}/.$$item; done
 	@for item in $(CONFIGS); do ln -vsfn ${BASE}/configs/$$item ${HOME}/.config/$$item; done
-	## link vim 
+	## link vim
 	@ln -vsfn ${BASE}/dotbase/nvim ${HOME}/.config/nvim
 	## link tmux
 	@ln -vsfn ${BASE}/dotbase/tmux ${HOME}/.tmux
 	@ln -vsf ${BASE}/dotbase/tmux/tmux.conf ${HOME}/.tmux.conf
-	## link fish 
+	## link fish
 	@ln -vsfn ${BASE}/dotbase/fish ${HOME}/.config/fish
 	@ln -vsfn ${BASE}/configs/fish/user.fish ${HOME}/.config/fish/user.fish
-	## config tmux 
+	## config tmux
 	@mkdir -pv ${HOME}/.tmux/plugins
 	@if [ ! -d "${HOME}/.tmux/plugins/tpm" ]; then \
 		git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm; \
 	fi
 
-update:
+update::
 	## update vim plugins
 	@nvim --headless "+Lazy! update" +qa
-	## update tmux plugins 
+	## update tmux plugins
 	~/.tmux/plugins/tpm/bin/install_plugins
 	~/.tmux/plugins/tpm/bin/update_plugins all
 	## update fish plugins
